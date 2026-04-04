8119090cb5bb1aad0e12eaaf0b2703295d0a3e62
Airat Abdulguzhin <utubutube29@gmail.com>
2026-04-04 15:00:34 +0500

    geosite update

--- gen.sh
+++ gen.sh
@@ -2,11 +2,16 @@
 cd /home/home/Projects/Go/domain-list-community
 TARGET_DIR="data"
 before=$(du -sh "$TARGET_DIR" | cut -f1)
-./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/apple
-./fetch_domains
-./fetch_domains
-./fetch_domains
-./fetch_domains
-./fetch_domains
+./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/category-ads
+./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/microsoft
+./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/private
+./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/roscomvpn-geosite
+./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/torrent
+./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/whitelist
+./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/win-spy
 after=$(du -sh "$TARGET_DIR" | cut -f1)
+./domain-list-community
 echo "Before: $before | After: $after"
+mv dlp.dat output/geosite.dat
+git commit -am "geosite update"
+git push
