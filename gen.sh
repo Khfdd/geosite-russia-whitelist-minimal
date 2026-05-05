15cb9f7e7508a4bd2d6997dc8e6c0bb83afdaa17
Airat Abdulguzhin <utubutube29@gmail.com>
2026-05-05 18:57:08 +0500

    geosite update

--- gen.sh
+++ gen.sh
@@ -10,9 +10,11 @@ before=$(du -sh "$TARGET_DIR" | cut -f1)
 ./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/whitelist
 ./fetch_domains https://raw.githubusercontent.com/hydraponique/roscomvpn-geosite/refs/heads/master/data/win-spy
 ./fetch_domains https://raw.githubusercontent.com/v2fly/domain-list-community/refs/heads/master/data/windsurf
+./fetch_domains https://raw.githubusercontent.com/v2fly/domain-list-community/refs/heads/master/data/category-ru
 after=$(du -sh "$TARGET_DIR" | cut -f1)
 ./domain-list-community
 echo "Before: $before | After: $after"
 mv dlc.dat output/geosite.dat
+git add "${TARGET_DIR}*"
 git commit -am "geosite update"
 git push
